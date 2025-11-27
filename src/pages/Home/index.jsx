09dc94c70fabd89827ad1674e274df4edcfd4fc3
Oliver <gustavovizis@gmail.com>
2025-11-27 12:14:23 -0300

    Inicializa projeto React: estrutura base e telas iniciais

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,38 @@
+import './style.css'
+import Logo from '../../assets/img/Logo.png'
+import Profile from '../../assets/img/Profile.png'
+
+function Home() {
+  return (
+    <main>
+
+      <section className='container-cadastro'>
+        <h1>Olá, Bem-Vindo!</h1>
+        <p>não tem uma conta?</p>
+        <img src={Profile} alt="Icon Perfil" />
+        <button type='button'>Cadastre-se</button>
+      </section>
+
+      <form className='container-form'>
+        <img src={Logo} alt="Logo do Colegio Benjamin Constant" />
+
+        <label htmlFor="email_user">E-mail Institucional</label>
+        <input type="email" id="email_user" placeholder='Digite seu e-mail' />
+
+        <label htmlFor="senha_user">Senha</label>
+        <input type="password" id="senha_user" placeholder='Digite sua senha' />
+
+        <div className="check">
+          <input type="checkbox" id="remember_me" />
+          <label htmlFor="remember_me">Relembre-me</label>
+        </div>
+
+        <a href="#">Esqueceu a senha?</a>
+        <button type='submit'>Entrar</button>
+      </form>
+
+    </main>
+  )
+}
+
+export default Home
